af1051e58ce0fd16929cfb0e774346d6e9a90085
boomika4020 <boomikabooms05@gmail.com>
2025-08-03 19:37:02 +0530

    Create StudentDetailApplication.java

---
+++ StudentDetailApplication.java
@@ -0,0 +1,13 @@
+package com.boomi.studentinfo.studentdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentdetailApplication.class, args);
+	}
+
+}
