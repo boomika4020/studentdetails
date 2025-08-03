72e88be5b5778a2b31deb94e36564934ef792a12
boomika4020 <boomikabooms05@gmail.com>
2025-08-03 19:35:56 +0530

    Create Studentservice.java

---
+++ Studentservice.java
@@ -0,0 +1,37 @@
+package com.boomi.studentinfo.studentdetail.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boomi.studentinfo.studentdetail.repository.StudentRepository;
+import com.boomi.studentinfo.studentdetail.model.Student;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repo;
+
+    public Student createStudent(Student student) {
+        return repo.save(student);
+    }
+    
+    public Student updateStudent(Student student) {
+    	return repo.save(student);
+    }
+
+    public List<Student> getAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Student> getById(int id) {
+        return repo.findById(id);
+    }
+
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
+}
